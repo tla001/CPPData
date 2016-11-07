ce1b817d6eb5f655fa7ff91fe5b54b62829b39c5
tla001 <1414419047@qq.com>
2016-11-07 10:39:28 +0800

    build

---
+++ CPPData/inc/Include.h
@@ -0,0 +1,16 @@
+/*
+ * Include.h
+ *
+ *  Created on: 2016Äê11ÔÂ7ÈÕ
+ *      Author: tla001
+ */
+
+#ifndef INC_INCLUDE_H_
+#define INC_INCLUDE_H_
+
+extern "C" {
+#include <stdio.h>
+#include <stdlib.h>
+}
+
+#endif /* INC_INCLUDE_H_ */
---
+++ CPPData/main.cpp
@@ -0,0 +1,12 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2016Äê11ÔÂ7ÈÕ
+ *      Author: tla001
+ */
+
+int main() {
+
+	return 0;
+}
+
